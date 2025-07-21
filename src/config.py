95f92104da7ab93ef8218b9da5ddf969862b8dad
Arthur Morais <arthurmorais.pereira@gmail.com>
2025-07-21 09:29:27 -0300

    (feat) adding config file

---
+++ src/config.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    OPENAI_API_KEY: Optional[str] = None
+    DEEPL_API_KEY: Optional[str] = None
+    GOOGLE_API_KEY: Optional[str] = None
+    GROQ_API_KEY: Optional[str] = None
+
+
+settings = Settings()
